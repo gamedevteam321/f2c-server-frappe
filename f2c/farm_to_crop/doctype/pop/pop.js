b3b799e973d61d4f6835631238e83a46d734f17a
vnsh kumar <vnsh@symbiotix.cloud>
2025-12-04 15:54:05 +0530

    Revert "FARM ACTIVITY MAPPING : Doctype Updated"
    This reverts commit c2c4a77e430952cf8847f56983e8ce2e1289d488.

--- f2c/farm_to_crop/doctype/pop/pop.js
+++ f2c/farm_to_crop/doctype/pop/pop.js
@@ -276,14 +276,6 @@ function show_activity_dialog(frm, activity_data, edit_idx) {
 				}
 			},
 			{
-				fieldname: 'category',
-				fieldtype: 'Select',
-				label: 'Category',
-				options: 'Compulsory\nOn Demand',
-				reqd: 1,
-				default: activity_data.category || 'Compulsory'
-			},
-			{
 				fieldname: 'section_remarks',
 				fieldtype: 'Section Break',
 				label: 'Additional Information'
@@ -310,7 +302,6 @@ function show_activity_dialog(frm, activity_data, edit_idx) {
 						dat: values.dat,
 						activity_group_type: values.activity_group_type,
 						activity: values.activity,
-						category: values.category,
 						remarks: values.remarks
 					}
 				},
